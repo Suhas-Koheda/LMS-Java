6a338fc18859f6a29a7e18b456ebad13a0bac63c
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2024-07-03 13:37:03 +0530

    chore:removal extra files

--- src/model/Assignment.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Assignment {
-    private int TotalMarks;
-    private int ScoredMarks;
-    private String Question;
-}
--- src/model/Faculty.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Faculty extends Member {
-    Faculty(){
-        super.setRole("Faculty");
-    }
-}
